f0215f08e2932a83a02940b8b1933267c64d80cf
Igor Nascimento <igor.nacimento51@gmail.com>
2024-05-07 15:26:19 -0300

    feat: add slf4j

--- src/main/java/com/crud/pokemon/service/user/UserService.java
+++ src/main/java/com/crud/pokemon/service/user/UserService.java
@@ -6,6 +6,7 @@ import com.crud.pokemon.model.User;
 import com.crud.pokemon.model.dto.users.*;
 import com.crud.pokemon.repository.UserRepository;
 import com.crud.pokemon.service.auth.AuthService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.http.ResponseEntity;
@@ -24,6 +25,7 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
+@Slf4j
 public class UserService implements UserDetailsService {
 
     @Autowired
@@ -51,6 +53,9 @@ public class UserService implements UserDetailsService {
         var usernamePassword = new UsernamePasswordAuthenticationToken(data.username(), data.password());
         var auth = this.authenticationManager.authenticate(usernamePassword);
         var token = authService.generateToken((User) auth.getPrincipal());
+
+        log.info("An user is authenticated!");
+
         return ResponseEntity.ok(new LoginResponseDTO(auth.getName(), token));
     }
     @Transactional
@@ -59,12 +64,18 @@ public class UserService implements UserDetailsService {
         User newUser = new User(data.name(), data.username(), encoder.encode(data.password()), data.role());
         this.userRepository.save(newUser);
         var token = authService.generateToken(newUser);
+
+        log.info("Registered an user!!");
+
         return ResponseEntity.ok().body(new LoginResponseDTO(data.username(), token));
     }
 
     @Transactional(readOnly = true)
     public List<UserDTO> getAllUsers() {
         List<User> userList = userRepository.findAll();
+
+        log.info("Finding all users!");
+
         return userList.stream()
                 .map(user -> new UserDTO(
                         user.getId(),
@@ -86,6 +97,9 @@ public class UserService implements UserDetailsService {
             user.setName(data.name());
             user.setUsername(data.username());
             user.setPassword(encoder.encode(data.password()));
+
+            log.info("Updating an user!");
+
             userRepository.save(user);
         });
     }
@@ -96,6 +110,7 @@ public class UserService implements UserDetailsService {
         if(user != null) {
             user.getWishlist().forEach(product -> product.getUsers().remove(user));
             userRepository.delete(user);
+            log.info("Deleting an user!");
         } else
             throw new NullUserException();
     }
